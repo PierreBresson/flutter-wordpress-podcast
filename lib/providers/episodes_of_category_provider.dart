d45386c75f0a4e8f34b1822a3ff0348ccf66e01f
Pierre Bresson <pierre@bresson.io>
2022-11-28 22:38:09 +0100

    episodes of a category working

--- lib/providers/episodes_of_category_provider.dart
+++ lib/providers/episodes_of_category_provider.dart
@@ -1,35 +1,24 @@
 import 'package:fwp/models/models.dart';
 import 'package:fwp/providers/http_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:tuple/tuple.dart';
 
-class PaginationIndexAndCategoryId {
-  final int pageIndex;
-  final int categoryId;
-
-  PaginationIndexAndCategoryId({
-    required this.pageIndex,
-    required this.categoryId,
-  });
-}
-
-final FutureProviderFamily<Episodes, PaginationIndexAndCategoryId>
+final FutureProviderFamily<Episodes, Tuple2<int, int>>
     paginatedEpisodesOfCategoryProvider =
-    FutureProvider.family<Episodes, PaginationIndexAndCategoryId>(
-        (ref, paginationAndCategoryId) async {
+    FutureProvider.family<Episodes, Tuple2<int, int>>((ref, item) async {
   final http = ref.watch(httpProvider);
   return http.getEpisodesFromCategory(
-    pageIndex: paginationAndCategoryId.pageIndex,
-    categoryId: paginationAndCategoryId.categoryId,
+    pageIndex: item.item1 + 1,
+    categoryId: item.item2,
   );
 });
 
-final ProviderFamily<AsyncValue<int>, PaginationIndexAndCategoryId>
+final ProviderFamily<AsyncValue<int>, Tuple2<int, int>>
     episodesOfCategoryCountProvider =
-    Provider.family<AsyncValue<int>, PaginationIndexAndCategoryId>(
-        (ref, paginationIndexAndCategoryId) {
+    Provider.family<AsyncValue<int>, Tuple2<int, int>>((ref, item) {
   return ref
       .watch(
-        paginatedEpisodesOfCategoryProvider(paginationIndexAndCategoryId),
+        paginatedEpisodesOfCategoryProvider(item.withItem1(1)),
       )
       .whenData((page) => page.total);
 });
--- lib/screens/episodes_of_category/episodes_of_category_screen.dart
+++ lib/screens/episodes_of_category/episodes_of_category_screen.dart
@@ -8,28 +8,16 @@ import 'package:fwp/repositories/repositories.dart';
 import 'package:fwp/widgets/widgets.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:macos_ui/macos_ui.dart';
+import 'package:tuple/tuple.dart';
 
-class EpisodesOfCategory extends StatefulWidget {
+class EpisodesOfCategory extends StatelessWidget {
   final ScrollController scrollController;
   const EpisodesOfCategory({required this.scrollController});
-
-  @override
-  State<EpisodesOfCategory> createState() => _EpisodesOfCategoryState();
-}
-
-class _EpisodesOfCategoryState extends State<EpisodesOfCategory> {
-  PaginationIndexAndCategoryId paginationIndexAndCategoryId =
-      PaginationIndexAndCategoryId(categoryId: 0, pageIndex: 0);
 
   @override
   Widget build(BuildContext context) {
     final EpisodesCategory episodesCategory =
         Beamer.of(context).currentBeamLocation.data! as EpisodesCategory;
-
-    paginationIndexAndCategoryId = PaginationIndexAndCategoryId(
-      pageIndex: 1,
-      categoryId: episodesCategory.id,
-    );
 
     return AdaptiveScaffold(
       titleBar: TitleBar(
@@ -49,16 +37,21 @@ class _EpisodesOfCategoryState extends State<EpisodesOfCategory> {
       body: HookConsumer(
         builder: (context, ref, child) {
           final count = ref.watch(
-            episodesOfCategoryCountProvider(paginationIndexAndCategoryId),
+            episodesOfCategoryCountProvider(
+              Tuple2<int, int>(0, episodesCategory.id),
+            ),
           );
 
           Future<Episodes> refresh() {
             ref.invalidate(
-              paginatedEpisodesOfCategoryProvider(paginationIndexAndCategoryId),
+              paginatedEpisodesOfCategoryProvider(
+                Tuple2<int, int>(0, episodesCategory.id),
+              ),
             );
             return ref.read(
-              paginatedEpisodesOfCategoryProvider(paginationIndexAndCategoryId)
-                  .future,
+              paginatedEpisodesOfCategoryProvider(
+                Tuple2<int, int>(0, episodesCategory.id),
+              ).future,
             );
           }
 
@@ -98,7 +91,7 @@ class _EpisodesOfCategoryState extends State<EpisodesOfCategory> {
               return RefreshIndicator(
                 onRefresh: refresh,
                 child: ListView.separated(
-                  controller: widget.scrollController,
+                  controller: scrollController,
                   separatorBuilder: (context, _) {
                     return const SizedBox(
                       height: 2,
@@ -108,10 +101,6 @@ class _EpisodesOfCategoryState extends State<EpisodesOfCategory> {
                   itemBuilder: (context, index) {
                     final pageIndex = index ~/ nbOfEpisodesPerPage;
                     final episodeIndex = index % nbOfEpisodesPerPage;
-                    final curr = PaginationIndexAndCategoryId(
-                      categoryId: episodesCategory.id,
-                      pageIndex: 1,
-                    );
 
                     return ProviderScope(
                       overrides: [
@@ -119,7 +108,8 @@ class _EpisodesOfCategoryState extends State<EpisodesOfCategory> {
                           ref
                               .watch(
                                 paginatedEpisodesOfCategoryProvider(
-                                  curr,
+                                  Tuple2<int, int>(
+                                      pageIndex, episodesCategory.id),
                                 ),
                               )
                               .whenData(
